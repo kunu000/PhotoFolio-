4e8655e3a8073a22fce19d9d7f508304b00e17d3
kunal Kirola <123616347+kunu000@users.noreply.github.com>
2023-08-03 08:32:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,8 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDnXaCLYlLpGz0YYsuhg8m0fzXBUF3dOLs",
-  authDomain: "photofolio-ed17a.firebaseapp.com",
-  projectId: "photofolio-ed17a",
-  storageBucket: "photofolio-ed17a.appspot.com",
-  messagingSenderId: "156404430368",
-  appId: "1:156404430368:web:70fd3add78b51c1811ee45",
-};
+// Enter Your web app's Firebase configuration below
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
